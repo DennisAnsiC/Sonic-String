2adff90a2474dcb4063183b2a9e02878d87c8642
DennisAnsiC <dennisansic@inbox.im>
2014-09-25 08:34:56 -0300

    Maldito tab kk

--- sstring.h
+++ sstring.h
@@ -3,95 +3,95 @@
 ** Sonic String.
 ** 
 ** Métodos (Public)...
-**	.value("Novo valor da string."); OK
-**		Seta novo valor na string (pode ser utilizado para inicialização).	
+**    .value("Novo valor da string."); OK
+**        Seta novo valor na string (pode ser utilizado para inicialização).    
 ** 
-**	.string(this); // OK
-**		Retorna string atual.
+**    .string(this); // OK
+**        Retorna string atual.
 **
-**	.length(this); // OK
-**		Retorna tamanho da string atual.
+**    .length(this); // OK
+**        Retorna tamanho da string atual.
 **
-**	.concat(this, " Concatena texto no final da string."); OK
-**		Concatena texto no final da string, caso não tiver dados setados, seta como '.value' (concatenação).
-** 	
-**	.compare(this, "Valor a ser comparado"); // OK
-**		Compara dados com o valor atual da string setado em '.value' (comparação).
+**    .concat(this, " Concatena texto no final da string."); OK
+**        Concatena texto no final da string, caso não tiver dados setados, seta como '.value' (concatenação).
+**     
+**    .compare(this, "Valor a ser comparado"); // OK
+**        Compara dados com o valor atual da string setado em '.value' (comparação).
 ** 
-**	.search(this, "Valor a ser procurado.); // OK
-**		Pesquisa dados dentro da string atual setada em '.value' (busca).
+**    .search(this, "Valor a ser procurado.); // OK
+**        Pesquisa dados dentro da string atual setada em '.value' (busca).
 **
-**	.delete(this); // OK
-**		Deleta string da memória (free).
+**    .delete(this); // OK
+**        Deleta string da memória (free).
 ** 
-**	.set_value(this, "novo", 6); //OK
-**		Substitui parte da string a partir de determinada posição.
+**    .set_value(this, "novo", 6); //OK
+**        Substitui parte da string a partir de determinada posição.
 **
-**	.replace(this, "velho", "novo");
-**		Substitui parte da string.
+**    .replace(this, "velho", "novo");
+**        Substitui parte da string.
 ** 
-**	.zero(this); // OK
-**		Zera string setando todos os valores para '\0'.
+**    .zero(this); // OK
+**        Zera string setando todos os valores para '\0'.
 ** 
-**	.split("++");
-**		Reparte string atual em pedaços de acordo com o delimitador.
+**    .split("++");
+**        Reparte string atual em pedaços de acordo com o delimitador.
 ** 
-**	.split_limit();
-**		Retorna quantidade de pedaços existentes (útil após uso de .split(..) para saber quantas strings existem).
-**	
-**	.split_value(1); <- 1, 2, 3, 4...
-**		Retorna valor da string que foi repartida.
-**	
-**	.split_length(1); <- 1, 2, 3, 4...
-**		Retorna tamanho atual da string repartida.
+**    .split_limit();
+**        Retorna quantidade de pedaços existentes (útil após uso de .split(..) para saber quantas strings existem).
+**    
+**    .split_value(1); <- 1, 2, 3, 4...
+**        Retorna valor da string que foi repartida.
+**    
+**    .split_length(1); <- 1, 2, 3, 4...
+**        Retorna tamanho atual da string repartida.
 **
-**	
-**	Métodos (Private)...
-**	.alloc (void* pointer, unsigned int length) // OK
-**		Aloca tamanho na memória.
-**		Retorno: void *
+**    
+**    Métodos (Private)...
+**    .alloc (void* pointer, unsigned int length) // OK
+**        Aloca tamanho na memória.
+**        Retorno: void *
 **
-**	.clear(void *this, unsigned int length); // OK
-**		Libera buffer da memória.
+**    .clear(void *this, unsigned int length); // OK
+**        Libera buffer da memória.
 **
-**	.len(const unsigned char *this); // OK
-**		Calcula tamanho da string e armazena resultado em 'string_length'.
-**		Retorno: unsigned int
-**	
-**	.set(void *pointer, unsigned int value, unsigned int length); // OK
-**		Altera valores de buffer na memória.
-**		Retorno: void *
-**	
-**	.copy(void *destination, const void *source, unsigned int length); // OK
-**		Copia buffer para outro buffer.
-**		Retorno: void *
-**	
-**	
-**	Tratamento de erros.
-**	.error(); // OK
-**		Retorna código do ultimo erro.
+**    .len(const unsigned char *this); // OK
+**        Calcula tamanho da string e armazena resultado em 'string_length'.
+**        Retorno: unsigned int
+**    
+**    .set(void *pointer, unsigned int value, unsigned int length); // OK
+**        Altera valores de buffer na memória.
+**        Retorno: void *
+**    
+**    .copy(void *destination, const void *source, unsigned int length); // OK
+**        Copia buffer para outro buffer.
+**        Retorno: void *
+**    
+**    
+**    Tratamento de erros.
+**    .error(); // OK
+**        Retorna código do ultimo erro.
 **
-**		Códigos de erro:
-**			SS_UNKNOWN = Erro desconhecido.
-**			SS_OK = String sem nenhum erro.
+**        Códigos de erro:
+**            SS_UNKNOWN = Erro desconhecido.
+**            SS_OK = String sem nenhum erro.
 **
 */
 
 /* Error Codes */
-#define SS_UNKNOWN				0x00
-#define SS_OK					0x01
-#define SS_DELETED				0x02
-#define SS_SET_VALUE			0x03
-#define SS_CONCATENATED			0x04
-#define SS_ZEROED				0x05
-#define SS_EQUAL				0x06
-#define SS_NOT_EQUAL			0x07
-#define SS_INVALID_BUFFER		0x08
-#define SS_STRING_FOUND			0x09
-#define SS_STRING_NOT_FOUND		0x10
-#define SS_VERY_LARGE_STRING	0x11
-#define SS_STRING_COPIED		0x12
-#define SS_REPLACED				0x13
+#define SS_UNKNOWN                 0x00
+#define SS_OK                      0x01
+#define SS_DELETED                 0x02
+#define SS_SET_VALUE               0x03
+#define SS_CONCATENATED            0x04
+#define SS_ZEROED                  0x05
+#define SS_EQUAL                   0x06
+#define SS_NOT_EQUAL               0x07
+#define SS_INVALID_BUFFER          0x08
+#define SS_STRING_FOUND            0x09
+#define SS_STRING_NOT_FOUND        0x10
+#define SS_VERY_LARGE_STRING       0x11
+#define SS_STRING_COPIED           0x12
+#define SS_REPLACED                0x13
 
 /* Types. (Public) */
 typedef unsigned int (*___error)(struct sstring *);
@@ -116,29 +116,29 @@ typedef void (*___clear)(void *, unsigned int);
 /* Structure of Sonic String. */
 typedef struct sstring
 {
-	int last_error;						// Armazena Código do ultimo erro.
-	unsigned char *string_value;		// Armazena Valor atual da string.
-	unsigned int string_length;			// Armazena Tamanho atual da string.
+    int last_error;                     // Armazena Código do ultimo erro.
+    unsigned char *string_value;        // Armazena Valor atual da string.
+    unsigned int string_length;         // Armazena Tamanho atual da string.
 
-	/* Public */
-	___error error;						// Retorna ultimo código de erro.
-	___string string;					// Retorna string atual.
-	___length length;					// Retorna tamanho da string.
-	___value value;						// Seta novo valor na string.
-	___delete delete;					// Deleta string da memória.
-	___concat concat;					// Concatena texto no final da string.
-	___zero zero;						// Seta todos os bytes da string para '\0';
-	___compare compare;					// Compara se strings são iguais.
-	___search search;					// Procura por string.
-	___set_value set_value;				// Substitui string a partir de uma determinada posição.
-	___replace replace;					// Substitui todas as ocorrencias encontradas em uma string.
+    /* Public */
+    ___error error;                      // Retorna ultimo código de erro.
+    ___string string;                    // Retorna string atual.
+    ___length length;                    // Retorna tamanho da string.
+    ___value value;                      // Seta novo valor na string.
+    ___delete delete;                    // Deleta string da memória.
+    ___concat concat;                    // Concatena texto no final da string.
+    ___zero zero;                        // Seta todos os bytes da string para '\0';
+    ___compare compare;                  // Compara se strings são iguais.
+    ___search search;                    // Procura por string.
+    ___set_value set_value;              // Substitui string a partir de uma determinada posição.
+    ___replace replace;                  // Substitui todas as ocorrencias encontradas em uma string.
 
-	/* Private */
-	___len len;							// Equivalente a strlen().
-	___set set;							// Equivalente a memset().
-	___copy copy;						// Equivalente a memcpy().
-	___alloc alloc;						// Equivalente a realloc().
-	___clear clear;						// Equivalente a free().
+    /* Private */
+    ___len len;                          // Equivalente a strlen().
+    ___set set;                          // Equivalente a memset().
+    ___copy copy;                        // Equivalente a memcpy().
+    ___alloc alloc;                      // Equivalente a realloc().
+    ___clear clear;                      // Equivalente a free().
 } sstring;
 
 /* Prototypes. (Checking) */
