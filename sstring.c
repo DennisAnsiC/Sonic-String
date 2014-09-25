af6f18f6d353151623413055b90337ff1f4e6014
DennisAnsiC <dennisansic@inbox.im>
2014-09-25 08:29:51 -0300

    Em desenvolvimento...
    Projeto ainda não terminado, upando apenas para mostrar para os amigos
    ;3

--- sstring.c
+++ sstring.c
@@ -1,52 +1,100 @@
 
 #include "sstring.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <Windows.h>	/* Use TRUE and FALSE. */
+#include <stdlib.h>		/* Use realloc and free. */
 
-/* Construtor */
-sstring * new_sstring (const unsigned char *this)
+/* Checking. */
+int sstring_isok(sstring *this)
 {
-	sstring *str = { 0 };
-
-	if ((str = (sstring *)malloc(sizeof(sstring))) != NULL)
-	{
-		/* Values. */
-		str->last_error		= SS_UNKNOWN;
-		str->string_value	= NULL;
-		str->string_length	= 0;
-
-		/* Public. */
-		str->error			= _error;
-		str->string			= _string;
-		str->length			= _length;
-		str->value			= _value;
-
-		/* Private. */
-		str->strlen			= _strlen;
-		str->memset			= _memset;
-		str->memcpy			= _memcpy;
-		str->alloc			= _alloc;
-
-		/* Is OK. */
-		str->last_error		= SS_OK;
-
-		/* Inicialização */
-		if (this != NULL)
-		{
-			str->value(str, this);
-		}
-	}
-
-	return str;
+	if (this != (sstring *)NULL)
+		if (this->error(this) == SS_OK)
+			return TRUE;
+		else
+			return this->error(this);
+	
+	return FALSE;
 }
 
-/* Funções. (Public) */
+int sstring_array_isok(sstring *this[], int limit)
+{
+	int control [2] = { 0, 0 };
+
+	if (limit > 0)
+	{
+		for (control[0] = 0; control[0] <= limit; control[0]++)
+		{
+			if (this[control[0]] != (sstring *)NULL)
+				if (this[control[0]]->error(this[control[0]]) == SS_OK)
+					control[1]++;					
+		}
+
+		if (control[1] == limit)
+			return TRUE;
+	}
+
+	return FALSE;
+}
+
+/* Constructor. */
+sstring *new_sstring (const unsigned char *string_initialization)
+{
+	sstring *string;
+
+	if ((string = (sstring *)smalloc(sizeof(sstring))) != NULL)
+	{
+		smemset(string, 0, sizeof(sstring));
+
+		/* Default values. */
+		string->last_error      = SS_UNKNOWN;
+		string->string_value    = NULL;
+		string->string_length   = 0;
+
+		/* Public. */
+		string->error           = _error;
+		string->string          = _string;
+		string->length          = _length;
+		string->value           = _value;
+		string->delete			= _delete;
+		string->concat			= _concat;
+		string->zero			= _zero;
+		string->compare			= _compare;
+		string->search			= _search;
+		string->set_value		= _set_value;
+		string->replace			= _replace;
+
+		/* Private. */
+		string->len             = _len;
+		string->set             = _set;
+		string->copy            = _copy;
+		string->alloc           = _alloc;
+		string->clear           = _clear;
+
+		/* Checking 'Is OK'. */
+		if (string->error  && string->string && string->length  && string->value  && string->delete    &&
+			string->concat && string->zero   && string->compare && string->search && string->set_value &&
+			string->replace &&
+			string->len    && string->set    && string->copy    && string->alloc  && string->clear)
+		{
+			string->last_error = SS_OK;
+
+			/* Initialization. */
+			if (string_initialization != (char *)NULL)
+				string->value(string, string_initialization);
+		}
+
+		return string;
+	}
+
+	return (sstring *)NULL;
+}
+
+/* Functions. (Public) */
 unsigned int _error(struct sstring *this)
 {
 	return this->last_error;
 }
 
-unsigned char * _string (struct sstring *this)
+unsigned char *_string (struct sstring *this)
 {
 	return this->string_value;
 }
@@ -56,65 +104,309 @@ unsigned int _length(struct sstring *this)
 	return this->string_length;
 }
 
-unsigned char * _value(sstring *this, const unsigned char *str)
+unsigned char *_value(sstring *this, const unsigned char *string)
 {
 	unsigned int length = 0;
+	this->last_error = SS_UNKNOWN;
 
-	if ((length = this->strlen(str)) > 0)
+	if ((length = this->len(string)) > 0)
 	{
-		if ((this->string_value = (char *)this->alloc(this->string_value, (length * sizeof(char)) + 1)) != NULL)
+		if ((this->string_value = (unsigned char *)this->alloc(this->string_value, 
+			(length * sizeof(char)) + 1)) != NULL)
 		{
-			this->memset((void *)this->string_value, '\0', (length * sizeof(char)) + 1);
-			this->memcpy((void *)this->string_value, (void *)str, length);
+			this->set((void *)this->string_value, '\0', (length * sizeof(char)) + 1);
+			this->copy((void *)this->string_value, (void *)string, length);
+			this->last_error = SS_SET_VALUE;
 			this->string_length = length;
 		}
 	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
 
 	return (unsigned char *)NULL;
 }
 
-/* Funções. (Private) */
-unsigned int _strlen(const unsigned char *string)
+void _delete(sstring *this)
 {
-	int length = 0;
+	if (this->string_value && this->string_length > 0)
+	{
+		this->clear(this->string_value, this->string_length);
+		this->last_error = SS_DELETED;
+		this->string_length = 0;
+	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
+}
 
-	while (string[length] != 0) 
+unsigned char *_concat(sstring *this, const unsigned char *string)
+{
+	unsigned char *pointer = (unsigned char *)string;
+	int length = 0, index = 0;
+	this->last_error = SS_UNKNOWN;
+
+	if (this->string_length > 0 && this->string_value && string)
+	{
+		if ((length = this->len(string)) > 0)
+		{
+			if ((this->string_value = (unsigned char *)this->alloc(this->string_value, 
+				((this->string_length + length) * sizeof(char)) + 1)) != NULL)
+			{
+				for (index = 0; index < length; index++)
+					this->string_value[this->string_length + index] = string[index];
+				
+				this->string_value[this->string_length + index] = '\0';
+				this->string_length += index;
+				
+				this->last_error = SS_CONCATENATED;
+				return this->string_value;
+			}
+		}
+	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
+
+	return (unsigned char *)NULL;
+}
+
+void _zero(sstring *this)
+{
+	if (this->string_value && this->string_length > 0)
+	{
+		this->set(this->string_value, '\0', this->string_length);
+		this->last_error = SS_ZEROED;
+	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
+}
+
+int _compare(sstring *this, const unsigned char *string)
+{
+	int control = 0;
+	this->last_error = SS_UNKNOWN;
+
+	if (string && this->string_value && this->string_length > 0)
+	{
+		while (string[control] == this->string_value[control])
+			control++;
+
+		if ((control - 1) == this->string_length && (control - 1) == this->len(string))
+			this->last_error = SS_EQUAL;
+		else
+			this->last_error = SS_NOT_EQUAL;
+	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
+
+	return this->last_error;
+}
+
+unsigned char *_search(struct sstring *this, const unsigned char *delimiter)
+{
+	unsigned char *pointer = NULL;
+	int control [4] = { 0, 0, 0, 0 };
+
+	if (delimiter && this->string_value && this->string_length > 0)
+	{
+		control[0] = this->len(delimiter);
+		pointer = this->string_value;
+
+		for (;*pointer++; control[3]++)
+		{
+			for (control[1] = 0; control[1] <= control[0]; control[1]++)
+				if (this->string_value[control[3] + control[1]] == delimiter[control[1]])
+					control[2]++;
+				else
+					break;
+			
+			if (control[2] == control[0]) 
+			{
+				this->last_error = SS_STRING_FOUND;
+				break;
+			}
+			else
+				this->last_error = SS_STRING_NOT_FOUND;
+		}
+
+		return pointer - 1;
+	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
+
+	return pointer;
+}
+
+unsigned char *_set_value(struct sstring *this, const unsigned char *string, unsigned int startup)
+{
+	int length = 0, index = 0;
+	this->last_error = SS_UNKNOWN;
+
+	if (string && this->string_value && this->string_length > 0)
+	{
+		length = this->len(string);
+		if ((startup + length) - 1 < this->string_length)
+		{
+			for (index = 0; index < length; index++)
+				this->string_value[startup + index] = string[index];
+			
+			this->last_error = SS_STRING_COPIED;
+			return &this->string_value[startup];
+		}
+		else
+			this->last_error = SS_VERY_LARGE_STRING;
+	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
+
+	return (unsigned char *)NULL;
+}
+
+unsigned char *_replace(struct sstring *this, const unsigned char *old_string, const unsigned char *new_string)
+{
+	unsigned char *pointer = NULL;
+	int control [6] = { 0, 0, 0, 0, 0, 0 };
+	this->last_error = SS_UNKNOWN;
+
+	if (old_string && new_string && this->string_value && this->string_length > 0)
+	{
+		control[0] = this->len(old_string);
+		control[4] = this->len(new_string);
+
+		if (control[4] < control[0] || control[4] == control[0])
+		{
+			if (control[4] < control[0]) 
+				control[5] = control[0] - control[4];
+			else
+				control[5] = 0;
+		}
+		else
+			control[5] = control[4] - control[0];
+			
+		for (;; control[3]++)
+		{
+			for (control[1] = 0; control[1] <= control[0]; control[1]++)
+			{
+				if (this->string_value[control[3] + control[1]] == '\0') break;
+				if (this->string_value[control[3] + control[1]] == old_string[control[1]])
+					control[2]++;
+				else
+					break;
+			}
+
+			if (control[2] == control[0]) 
+			{
+				// FIXME
+				if (control[4] > control[0])
+				{
+					this->string_value = (unsigned char *)this->alloc(this->string_value, 
+						((this->string_length + control[5]) * sizeof(char)) + 1);
+
+					for (control[1] = control[3] + control[4]; this->string_value[control[1]] != '\0'; control[1]++)
+						this->string_value[control[3] + control[0]] = this->string_value[control[1]];
+
+					this->set_value(this, new_string, control[3]);
+				}
+				else
+				{
+					this->set_value(this, new_string, control[3]);
+					for (control[1] = control[3] + control[4]; this->string_value[control[1]] != '\0'; control[1]++)
+					{
+						this->string_value[control[1]] = this->string_value[control[1] + control[5]];
+					}
+				}
+
+				if (this->string_length > (unsigned int)(control[3] + control[4]))
+				{
+					control[3] += control[4];
+					control[2] = 0;
+				}
+				else
+					break;
+			}
+		}
+	}
+	else
+		this->last_error = SS_INVALID_BUFFER;
+
+	return pointer;
+}
+
+/* Functions. (Private) */
+unsigned int _len(const unsigned char *string)
+{
+	unsigned int length = 0;
+
+	while (*string++)
 		length++;
 
 	return length;
 }
 
-void *_memset(void *pointer, unsigned int value, unsigned int size)
+void *_set(void *pointer, unsigned int value, unsigned int length)
 {
-	unsigned char *_ptr = (unsigned char *)pointer;
+	unsigned char *_pointer = (unsigned char *)pointer;
 
-	while (size > 0)
+	while (length > 0)
 	{
-		*_ptr = value;
-		_ptr++;
-		size--;
+		*_pointer = value;
+		_pointer++;
+		length--;
 	}
 
-	return (void *)_ptr;
+	return (void *)_pointer;
 }
 
-void *_memcpy(void *destination, const void *source, unsigned int size)
+void *_copy(void *destination, const void *source, unsigned int length)
 {
-	unsigned char *ptr_a = (unsigned char *)destination;
-	unsigned char *ptr_b = (unsigned char *)source;
+	unsigned char *pointer_a = (unsigned char *)destination;
+	unsigned char *pointer_b = (unsigned char *)source;
 
-	while (size--)
-		*ptr_a++ = *ptr_b++;
+	while (length--)
+		*pointer_a++ = *pointer_b++;
 
 	return destination;
 }
 
-void* _alloc (void* pointer, unsigned int size)
+void *_alloc (void* pointer, unsigned int length)
 {
-	void *ptr;
+	void *_pointer;
 
-	if ((ptr = realloc(pointer, size)) != NULL)
-		return ptr;
+	if ((_pointer = realloc(pointer, length)) != NULL)
+		return _pointer;
 
 	return NULL;
+}
+
+void _clear(void *pointer, unsigned int length)
+{
+	smemset(pointer, '\0', length);
+	free(pointer);
+}
+
+/* Functions. (Extras) */
+void *smalloc(unsigned int length)
+{
+	void *pointer;
+
+	if (length)
+	{
+		pointer = malloc(length);
+		if (pointer) 
+			return pointer;		
+	}
+
+	return NULL;
+}
+
+void *smemset(void *pointer, unsigned int value, unsigned int length)
+{
+	unsigned char *_pointer = (unsigned char *)pointer;
+
+	while (length > 0)
+	{
+		*_pointer = value;
+		_pointer++;
+		length--;
+	}
+
+	return pointer;
 }
--- sstring.h
+++ sstring.h
@@ -12,22 +12,25 @@
 **	.length(this); // OK
 **		Retorna tamanho da string atual.
 **
-**	.concat(" Concatena texto no final da string.");
+**	.concat(this, " Concatena texto no final da string."); OK
 **		Concatena texto no final da string, caso não tiver dados setados, seta como '.value' (concatenação).
 ** 	
-**	.compare("Valor a ser comparado");
+**	.compare(this, "Valor a ser comparado"); // OK
 **		Compara dados com o valor atual da string setado em '.value' (comparação).
 ** 
-**	.search("Valor a ser procurado.);
+**	.search(this, "Valor a ser procurado.); // OK
 **		Pesquisa dados dentro da string atual setada em '.value' (busca).
 **
-**	.delete();
+**	.delete(this); // OK
 **		Deleta string da memória (free).
 ** 
-**	.set_value("Novo", 0, 4);
-**		Seta valor em determinada parte da string.
+**	.set_value(this, "novo", 6); //OK
+**		Substitui parte da string a partir de determinada posição.
+**
+**	.replace(this, "velho", "novo");
+**		Substitui parte da string.
 ** 
-**	.zero(this);
+**	.zero(this); // OK
 **		Zera string setando todos os valores para '\0'.
 ** 
 **	.split("++");
@@ -44,82 +47,127 @@
 **
 **	
 **	Métodos (Private)...
-**	.alloc (void* pointer, unsigned int size) // OK
+**	.alloc (void* pointer, unsigned int length) // OK
 **		Aloca tamanho na memória.
 **		Retorno: void *
 **
-**	.clear(void *this);
+**	.clear(void *this, unsigned int length); // OK
 **		Libera buffer da memória.
 **
-**	.strlen(const unsigned char *this); // OK
+**	.len(const unsigned char *this); // OK
 **		Calcula tamanho da string e armazena resultado em 'string_length'.
 **		Retorno: unsigned int
 **	
-**	.memset(void *ptr, unsigned int value, unsigned int size); // OK
+**	.set(void *pointer, unsigned int value, unsigned int length); // OK
 **		Altera valores de buffer na memória.
 **		Retorno: void *
 **	
-**	.memcpy(void *destination, const void *source, unsigned int size); // OK
+**	.copy(void *destination, const void *source, unsigned int length); // OK
 **		Copia buffer para outro buffer.
 **		Retorno: void *
 **	
 **	
 **	Tratamento de erros.
-**	.error();
+**	.error(); // OK
 **		Retorna código do ultimo erro.
 **
 **		Códigos de erro:
+**			SS_UNKNOWN = Erro desconhecido.
 **			SS_OK = String sem nenhum erro.
 **
 */
 
-/* Códigos de erro */
-#define SS_UNKNOWN	0x00
-#define SS_OK		0x01
+/* Error Codes */
+#define SS_UNKNOWN				0x00
+#define SS_OK					0x01
+#define SS_DELETED				0x02
+#define SS_SET_VALUE			0x03
+#define SS_CONCATENATED			0x04
+#define SS_ZEROED				0x05
+#define SS_EQUAL				0x06
+#define SS_NOT_EQUAL			0x07
+#define SS_INVALID_BUFFER		0x08
+#define SS_STRING_FOUND			0x09
+#define SS_STRING_NOT_FOUND		0x10
+#define SS_VERY_LARGE_STRING	0x11
+#define SS_STRING_COPIED		0x12
+#define SS_REPLACED				0x13
 
-/* Tipos. (Public) */
+/* Types. (Public) */
 typedef unsigned int (*___error)(struct sstring *);
-typedef unsigned char * (*___string)(struct sstring *);
+typedef unsigned char *(*___string)(struct sstring *);
 typedef unsigned int (*___length)(struct sstring *);
-typedef unsigned char * (*___value)(struct sstring *, const unsigned char *);
+typedef unsigned char *(*___value)(struct sstring *, const unsigned char *);
+typedef void (*___delete)(struct sstring *);
+typedef unsigned char *(*___concat)(struct sstring *, const unsigned char *);
+typedef void (*___zero)(struct sstring *);
+typedef int (*___compare)(struct sstring *, const unsigned char *);
+typedef unsigned char *(*___search)(struct sstring *, const unsigned char *);
+typedef unsigned char *(*___set_value)(struct sstring *, const unsigned char *, unsigned int);
+typedef unsigned char *(*___replace)(struct sstring *, const unsigned char *, const unsigned char *);
 
-/* Tipos. (Private) */
-typedef unsigned int (*___strlen)(const unsigned char *);
-typedef void * (*___memset)(void *, unsigned int, unsigned int);
-typedef void * (*___memcpy)(void *, const void *, unsigned int);
-typedef void * (*___alloc)(void *, unsigned int);
+/* Types. (Private) */
+typedef unsigned int (*___len)(const unsigned char *);
+typedef void *(*___set)(void *, unsigned int, unsigned int);
+typedef void *(*___copy)(void *, const void *, unsigned int);
+typedef void *(*___alloc)(void *, unsigned int);
+typedef void (*___clear)(void *, unsigned int);
 
-/* Estrutura da Sonic String. */
+/* Structure of Sonic String. */
 typedef struct sstring
 {
-	int last_error;				// Armazena Código do ultimo erro.
-	char *string_value;			// Armazena Valor atual da string.
-	int string_length;			// Armazena Tamanho atual da string.
+	int last_error;						// Armazena Código do ultimo erro.
+	unsigned char *string_value;		// Armazena Valor atual da string.
+	unsigned int string_length;			// Armazena Tamanho atual da string.
 
 	/* Public */
-	___error error;				// Retorna ultimo código de erro.
-	___string string;			// Retorna string atual.
-	___length length;			// Retorna tamanho da string.
-	___value value;				// Seta novo valor na string.
+	___error error;						// Retorna ultimo código de erro.
+	___string string;					// Retorna string atual.
+	___length length;					// Retorna tamanho da string.
+	___value value;						// Seta novo valor na string.
+	___delete delete;					// Deleta string da memória.
+	___concat concat;					// Concatena texto no final da string.
+	___zero zero;						// Seta todos os bytes da string para '\0';
+	___compare compare;					// Compara se strings são iguais.
+	___search search;					// Procura por string.
+	___set_value set_value;				// Substitui string a partir de uma determinada posição.
+	___replace replace;					// Substitui todas as ocorrencias encontradas em uma string.
 
 	/* Private */
-	___strlen strlen;			// Equivalente a strlen().
-	___memset memset;			// Equivalente a memset().
-	___memcpy memcpy;			// Equivalente a memcpy().
-	___alloc  alloc;			// Equivalente a realloc().
+	___len len;							// Equivalente a strlen().
+	___set set;							// Equivalente a memset().
+	___copy copy;						// Equivalente a memcpy().
+	___alloc alloc;						// Equivalente a realloc().
+	___clear clear;						// Equivalente a free().
 } sstring;
 
-/* Construtor. */
-sstring * new_sstring (const unsigned char *this);
+/* Prototypes. (Checking) */
+int sstring_isok(sstring *this);
+int sstring_array_isok(sstring *this[], int limit);
 
-/* Protótipos. (Public) */
+/* Prototypes. (Constructor) */
+sstring *new_sstring(const unsigned char *string_initialization);
+
+/* Prototypes. (Public) */
 unsigned int _error(sstring *this);
-unsigned char * _string (sstring *this);
+unsigned char *_string(sstring *this);
 unsigned int _length(sstring *this);
-unsigned char * _value(sstring *this, const unsigned char *str);
+unsigned char *_value(sstring *this, const unsigned char *string);
+void _delete(sstring *this);
+unsigned char *_concat(sstring *this, const unsigned char *string);
+void _zero(sstring *this);
+int _compare(sstring *this, const unsigned char *string);
+unsigned char *_search(struct sstring *this, const unsigned char *delimiter);
+unsigned char *_set_value(struct sstring *this, const unsigned char *string, unsigned int startup);
+unsigned char *_replace(struct sstring *this, const unsigned char *old_string, const unsigned char *new_string);
 
-/* Protótipos. (Private) */
-unsigned int _strlen(const unsigned char *string);
-void *_memset(void *pointer, unsigned int value, unsigned int size);
-void *_memcpy(void *destination, const void *source, unsigned int size);
-void* _alloc (void* pointer, unsigned int size);
+/* Prototypes. (Private) */
+unsigned int _len(const unsigned char *string);
+void *_set(void *pointer, unsigned int value, unsigned int length);
+void *_copy(void *destination, const void *source, unsigned int length);
+void *_alloc(void* pointer, unsigned int length);
+void _clear(void *pointer, unsigned int length);
+
+/* Prototypes. (Extras) */
+void *smalloc(unsigned int length);
+void *smemset(void *pointer, unsigned int value, unsigned int length);
