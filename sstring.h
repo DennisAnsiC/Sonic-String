07a6f66a99b2053a92b6f86b0879551b4c38ac2f
dennisansic <dennisansic@inbox.im>
2014-09-27 02:59:57 -0300

    Last update

--- sstring.h
+++ sstring.h
@@ -1,23 +1,36 @@
 
-/* 
-** Sonic String.
-*/
+/* Defines */
+#ifndef NULL
+#define NULL                    ((void *)0)
+#endif
+
+#ifndef FALSE
+#define FALSE                   0
+#endif
+
+#ifndef TRUE
+#define TRUE                    1
+#endif
 
 /* Error Codes */
-#define SS_UNKNOWN                 0x00
-#define SS_OK                      0x01
-#define SS_DELETED                 0x02
-#define SS_SET_VALUE               0x03
-#define SS_CONCATENATED            0x04
-#define SS_ZEROED                  0x05
-#define SS_EQUAL                   0x06
-#define SS_NOT_EQUAL               0x07
-#define SS_INVALID_BUFFER          0x08
-#define SS_STRING_FOUND            0x09
-#define SS_STRING_NOT_FOUND        0x10
-#define SS_VERY_LARGE_STRING       0x11
-#define SS_STRING_COPIED           0x12
-#define SS_REPLACED                0x13
+#define SS_UNKNOWN              0x00
+#define SS_OK                   0x01
+#define SS_DELETED              0x02
+#define SS_SET_VALUE            0x03
+#define SS_CONCATENATED         0x04
+#define SS_ZEROED               0x05
+#define SS_EQUAL                0x06
+#define SS_NOT_EQUAL            0x07
+#define SS_INVALID_BUFFER       0x08
+#define SS_STRING_FOUND         0x09
+#define SS_STRING_NOT_FOUND     0x10
+#define SS_VERY_LARGE_STRING    0x11
+#define SS_STRING_COPIED        0x12
+#define SS_REPLACED             0x13
+#define SS_TOLOWER_EXECUTED     0x14
+#define SS_TOUPPER_EXECUTED     0x15
+#define SS_POSITION_FOUND       0x16
+#define SS_STRING_REVERTED      0x17
 
 /* Types. (Public) */
 typedef unsigned int (*___error)(struct sstring *);
@@ -27,10 +40,16 @@ typedef unsigned char *(*___value)(struct sstring *, const unsigned char *);
 typedef void (*___delete)(struct sstring *);
 typedef unsigned char *(*___concat)(struct sstring *, const unsigned char *);
 typedef void (*___zero)(struct sstring *);
-typedef int (*___compare)(struct sstring *, const unsigned char *);
+typedef unsigned int (*___compare)(struct sstring *, const unsigned char *);
 typedef unsigned char *(*___search)(struct sstring *, const unsigned char *);
 typedef unsigned char *(*___set_value)(struct sstring *, const unsigned char *, unsigned int);
 typedef unsigned char *(*___replace)(struct sstring *, const unsigned char *, const unsigned char *);
+typedef unsigned char *(*___tolower)(struct sstring *);
+typedef unsigned char *(*___toupper)(struct sstring *);
+typedef unsigned int (*___position)(struct sstring *, const unsigned char *);
+typedef unsigned int (*___is_char)(const char);
+typedef unsigned int (*___is_numeric)(const char);
+typedef unsigned char *(*___revert)(struct sstring *);
 
 /* Types. (Private) */
 typedef unsigned int (*___len)(const unsigned char *);
@@ -42,29 +61,35 @@ typedef void (*___clear)(void *, unsigned int);
 /* Structure of Sonic String. */
 typedef struct sstring
 {
-    int last_error;                     // Armazena Código do ultimo erro.
-    unsigned char *string_value;        // Armazena Valor atual da string.
-    unsigned int string_length;         // Armazena Tamanho atual da string.
+   int _last_error;                   // Armazena Código do ultimo erro.
+   unsigned char *_string_value;      // Armazena Valor atual da string.
+   unsigned int _string_length;       // Armazena Tamanho atual da string.
 
-    /* Public */
-    ___error error;                      // Retorna ultimo código de erro.
-    ___string string;                    // Retorna string atual.
-    ___length length;                    // Retorna tamanho da string.
-    ___value value;                      // Seta novo valor na string.
-    ___delete delete;                    // Deleta string da memória.
-    ___concat concat;                    // Concatena texto no final da string.
-    ___zero zero;                        // Seta todos os bytes da string para '\0';
-    ___compare compare;                  // Compara se strings são iguais.
-    ___search search;                    // Procura por string.
-    ___set_value set_value;              // Substitui string a partir de uma determinada posição.
-    ___replace replace;                  // Substitui todas as ocorrencias encontradas em uma string.
+   /* Public */
+   ___error error;                    // Retorna ultimo código de erro.
+   ___string string;                  // Retorna string atual.
+   ___length length;                  // Retorna tamanho da string.
+   ___value value;                    // Seta novo valor na string.
+   ___delete delete;                  // Deleta string da memória.
+   ___concat concat;                  // Concatena texto no final da string.
+   ___zero zero;                      // Seta todos os bytes da string para '\0';
+   ___compare compare;                // Compara se strings são iguais.
+   ___search search;                  // Procura por string.
+   ___set_value set_value;            // Substitui string a partir de uma determinada posição.
+   ___replace replace;                // Substitui todas as ocorrencias encontradas em uma string.
+   ___tolower tolower;                // Converte string para minuscula.
+   ___toupper toupper;                // Converte string para maiuscula.
+   ___position position;              // Retorna posição do delimitador na string.
+   ___is_char is_char;                // Verifica se caracter é alguma letra do alfabeto.
+   ___is_numeric is_numeric;          // Verifica se caracter é um número decimal.
+   ___revert revert;                  // Reverte ordem dos caracteres da string.
 
-    /* Private */
-    ___len len;                          // Equivalente a strlen().
-    ___set set;                          // Equivalente a memset().
-    ___copy copy;                        // Equivalente a memcpy().
-    ___alloc alloc;                      // Equivalente a realloc().
-    ___clear clear;                      // Equivalente a free ().
+   /* Private */
+   ___len _len;                       // Equivalente a strlen().
+   ___set _set;                       // Equivalente a memset().
+   ___copy _copy;                     // Equivalente a memcpy().
+   ___alloc _alloc;                   // Equivalente a realloc().
+   ___clear _clear;                   // Equivalente a free().
 } sstring;
 
 /* Prototypes. (Checking) */
@@ -83,9 +108,15 @@ void _delete(sstring *this);
 unsigned char *_concat(sstring *this, const unsigned char *string);
 void _zero(sstring *this);
 int _compare(sstring *this, const unsigned char *string);
-unsigned char *_search(struct sstring *this, const unsigned char *delimiter);
-unsigned char *_set_value(struct sstring *this, const unsigned char *string, unsigned int startup);
-unsigned char *_replace(struct sstring *this, const unsigned char *old_string, const unsigned char *new_string);
+unsigned char *_search(sstring *this, const unsigned char *delimiter);
+unsigned char *_set_value(sstring *this, const unsigned char *string, unsigned int startup);
+unsigned char *_replace(sstring *this, const unsigned char *old_string, const unsigned char *new_string);
+unsigned char *__tolower(sstring *this);
+unsigned char *__toupper(sstring *this);
+unsigned int _position(sstring *this, const unsigned char *delimiter);
+unsigned int _is_char(const char _char);
+unsigned int _is_numeric(const char _char);
+unsigned char *_revert(sstring *this);
 
 /* Prototypes. (Private) */
 unsigned int _len(const unsigned char *string);
@@ -93,7 +124,6 @@ void *_set(void *pointer, unsigned int value, unsigned int length);
 void *_copy(void *destination, const void *source, unsigned int length);
 void *_alloc(void* pointer, unsigned int length);
 void _clear(void *pointer, unsigned int length);
-
-/* Prototypes. (Extras) */
-void *smalloc(unsigned int length);
-void *smemset(void *pointer, unsigned int value, unsigned int length);
+unsigned char *_search_ex(unsigned char *string, const unsigned char *delimiter, unsigned int *last_error, unsigned int *position);
+unsigned char *_str_to_lower_and_upper(unsigned char *string, const unsigned int length,
+   const unsigned int start_a, const unsigned int start_b, const unsigned int delimiter);
--- tests.c
+++ tests.c
@@ -1,135 +1,159 @@
 
-/* Testing... */
-
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <windows.h>
 #include "sstring.h"
 
 int main (int argc, char *argv[])
 {
-    int a;
-    unsigned char *ptr;
+   int a;
+   unsigned char *ptr;
 
-    // String array...
-    sstring *str[] =
-    {
-        new_sstring("Valor 1"),
-        new_sstring("Meu Valor 2 com tamanho grande!"),
-        new_sstring(NULL),
-        NULL
-    };
+   // String array...
+   sstring *str[] =
+   {
+      new_sstring("Valor 1"),
+      new_sstring("Meu Valor 2 com tamanho grande!"),
+      new_sstring(NULL),
+      NULL                  // <-- Terminador NULL obrigatório.
+   };
 
-    // Normal...
-    sstring *stra = new_sstring(NULL);
+   // Normal...
+   sstring *stra = new_sstring(NULL);
 
-    // String array test...    
-    if (sstring_array_isok(str, 3))
-    {
-        printf("str: %s\nlen: %d\n", str[0]->string(str[0]), str[0]->length(str[0]));
-        str[0]->value(str[0], "Novo valor 1");
-        printf("str: %s\nlen: %d\n", str[0]->string(str[0]), str[0]->length(str[0]));
+   // String array test...   
+   if (sstring_array_isok(str, 3))
+   {
+      printf("str: %s\nlen: %d\n", str[0]->string(str[0]), str[0]->length(str[0]));
+      str[0]->value(str[0], "Novo valor 1");
+      printf("str: %s\nlen: %d\n", str[0]->string(str[0]), str[0]->length(str[0]));
 
-        printf("str: %s\nlen: %d\n", str[1]->string(str[1]), str[1]->length(str[1]));
-        str[1]->value(str[1], "Small");
-        printf("str: %s\nlen: %d\n", str[1]->string(str[1]), str[1]->length(str[1]));
+      printf("str: %s\nlen: %d\n", str[1]->string(str[1]), str[1]->length(str[1]));
+      str[1]->value(str[1], "Small");
+      printf("str: %s\nlen: %d\n", str[1]->string(str[1]), str[1]->length(str[1]));
 
-        str[2]->value(str[2], "Valor 3");
-        printf("str: %s\nlen: %d\n", str[2]->string(str[2]), str[2]->length(str[2]));
+      str[2]->value(str[2], "Valor 3");
+      printf("str: %s\nlen: %d\n", str[2]->string(str[2]), str[2]->length(str[2]));
 
-        // Deleta string da memória.
-        str[0]->delete(str[0]);
-        str[1]->delete(str[1]);
-        str[2]->delete(str[2]);
+      // Deleta string da memória.
+      str[0]->delete(str[0]);
+      str[1]->delete(str[1]);
+      str[2]->delete(str[2]);
 
-        for (a = 0; a <= 2; a++)
-            printf("Error code: 0x%d\n", str[a]->error(str[a]));
+      for (a = 0; a <= 2; a++)
+         printf("Error code: 0x%d\n", str[a]->error(str[a]));
 
-        printf("\n");
-    }
-    else
-    {
-        if (stra != (sstring *)NULL)
-            printf("Error code: 0x%d\n", stra->error(stra));
-        else 
-            printf("Struct is NULL!\n");
-    }
+      printf("\n");
+   }
+   else
+   {
+      if (stra != (sstring *)NULL)
+         printf("Error code: 0x%d\n", stra->error(stra));
+      else 
+         printf("Struct is NULL!\n");
+   }
 
-    if (sstring_isok(stra) == TRUE)
-    {
-        // Atribuição e alteração de valores.
-        stra->value(stra, "Minha String normal!");
-        printf("str: %s\nlen: %d\n", stra->string(stra), stra->length(stra));
+   if (sstring_isok(stra) == TRUE)
+   {
+      // Atribuição e alteração de valores.
+      stra->value(stra, "Minha String normal!");
+      printf("str: %s\nlen: %d\n", stra->string(stra), stra->length(stra));
 
-        stra->value(stra, "Minha String normal com tamanho um pouco maior...");
-        printf("str: %s\nlen: %d\n", stra->string(stra), stra->length(stra));
+      stra->value(stra, "Minha String normal com tamanho um pouco maior...");
+      printf("str: %s\nlen: %d\n", stra->string(stra), stra->length(stra));
 
-        stra->value(stra, "Minha String normal com tamanho menor...");
-        printf("str: %s\nlen: %d\n", stra->string(stra), stra->length(stra));
-        stra->value(stra, "essa eh minha string normal");
+      stra->value(stra, "Minha String normal com tamanho menor...");
+      printf("str: %s\nlen: %d\n", stra->string(stra), stra->length(stra));
+      stra->value(stra, "essa eh minha string normal");
 
-        // Concatenação.
-        printf("ptr: %s - len: %d\n", stra->concat(stra, " e isso foi concatenado no final da string."), stra->length(stra));
-        printf("str: %s - len: %d\n", stra->string(stra), stra->length(stra));
+      // Concatenação.
+      printf("ptr: %s - len: %d\n", stra->concat(stra, " e isso foi concatenado no final da string."), stra->length(stra));
+      printf("str: %s - len: %d\n", stra->string(stra), stra->length(stra));
 
-        // Seta todos os bytes da string para '\0'
-        stra->zero(stra);
-        printf("String zerada.\nError code: 0x%d\n", stra->error(stra));
+      // Seta todos os bytes da string para '\0'
+      stra->zero(stra);
+      printf("String zerada.\nError code: 0x%d\n", stra->error(stra));
 
-        stra->value(stra, "Minha string com valor novo.");
-        printf("str: %s - len: %d\n", stra->string(stra), stra->length(stra));
+      stra->value(stra, "Minha string com valor novo.");
+      printf("str: %s - len: %d\n", stra->string(stra), stra->length(stra));
 
-        // Comparação.
-        stra->value(stra, "Minha string XXX");
-        printf("str: %s - len: %d\n", stra->string(stra), stra->length(stra));
+      // Comparação.
+      stra->value(stra, "Minha string XXX");
+      printf("str: %s - len: %d\n", stra->string(stra), stra->length(stra));
 
-        if (stra->compare(stra, "Minha string XXX") == SS_EQUAL)
-            printf("Strings sao iguais!\n");
-        else
-            printf("Strings diferentes.\nError code: 0x%d\n\n", stra->error(stra));
-        printf("\n");
+      if (stra->compare(stra, "Minha string XXX") == SS_EQUAL)
+         printf("Strings sao iguais!\n");
+      else
+         printf("Strings diferentes.\nError code: 0x%d\n\n", stra->error(stra));
+      printf("\n");
 
-        // Procura / Search.
-        if ((ptr = stra->search(stra, "string")) != NULL)
-        {
-            if (stra->error(stra) == SS_STRING_FOUND)
-                printf("String encontrada\nstr: %s\nptr: %s\nError code: 0x%d\n", stra->string(stra), ptr, stra->error(stra));
-            else if (stra->error(stra) == SS_STRING_NOT_FOUND)
-                printf("String nao encontrada.\nError code: 0x%d\n\n", stra->error(stra));
-            else
-                printf("Erro desconhecido.\nError code: 0x%d\n\n", stra->error(stra));
-        }
-        else
+      // Procura / Search.
+      if ((ptr = stra->search(stra, "string")) != NULL)
+      {
+         if (stra->error(stra) == SS_STRING_FOUND)
+            printf("String encontrada\nstr: %s\nptr: %s\nError code: 0x%d\n", stra->string(stra), ptr, stra->error(stra));
+         else if (stra->error(stra) == SS_STRING_NOT_FOUND)
             printf("String nao encontrada.\nError code: 0x%d\n\n", stra->error(stra));
+         else
+            printf("Erro desconhecido.\nError code: 0x%d\n\n", stra->error(stra));
+      }
+      else
+         printf("String nao encontrada.\nError code: 0x%d\n\n", stra->error(stra));
 
-        // Substitui parte da string após determinada posição.
-        stra->value(stra, "Minha velha string XX.");
-        printf("\n\nstr: %s - len: %d - Error code: 0x%d\n", stra->string(stra), stra->length(stra), stra->error(stra));
-        
-        printf("\nptr: %s\n", stra->set_value(stra, "YYY", 19));
-        printf("\nstr: %s - len: %d - Error code: 0x%d\n", stra->string(stra), stra->length(stra), stra->error(stra));
+      // Substitui parte da string após determinada posição.
+      stra->value(stra, "Minha velha string XX.");
+      printf("\n\nstr: %s - len: %d - Error code: 0x%d\n", stra->string(stra), stra->length(stra), stra->error(stra));
+      
+      printf("\nptr: %s\n", stra->set_value(stra, "YYY", 19));
+      printf("\nstr: %s - len: %d - Error code: 0x%d\n", stra->string(stra), stra->length(stra), stra->error(stra));
+      
+      // Converte strings para minuscula.
+      stra->value(stra, "MINHA STRING EM MAIUSCULA!");
+      printf("%s\n", stra->string(stra));
+      printf("%s\n", stra->tolower(stra));
 
-        // Substitui todas as ocorrencias em uma string.
-        stra->value(stra, "velhaxx Minha velhaxx string velhaxx porra velhaxx llalala velhaxx");
-        printf("\n\nstr: %s - len: %d - Error code: 0x%d\n", stra->string(stra), stra->length(stra), stra->error(stra));
+      // Converte strings para maiuscula.
+      stra->value(stra, "minha string em minuscula!");
+      printf("%s\n", stra->string(stra));
+      printf("%s\n", stra->toupper(stra));
 
-        // velhaxx
-        // novaxxx
-        //stra->replace(stra, "velhaxx", "novaxxx");
-        //printf("replace...\nstr: %s - len: %d - Error code: 0x%d\n", stra->string(stra), stra->length(stra), stra->error(stra));
+      // Retorna posição do delimitador na string atual.
+      stra->value(stra, "minha string em minuscula!");
+      printf("str:%s - %d\n", stra->string(stra), stra->position(stra, "em"));
 
-        // Deleta string
-        stra->delete(stra);
-        printf("Error code: 0x%d\n\n", stra->error(stra));
-    }
-    else
-    {
-        if (stra != (sstring *)NULL)
-            printf("Error code: 0x%d\n", stra->error(stra));
-        else 
-            printf("Struct is NULL!\n");
-    }
+      // Verifica se é um caracter.
+      if (stra->is_char('a'))
+         printf("Eh uma letra\n");
 
-    return 0;
+      if (stra->is_char('1'))
+         printf("Eh uma letra\n");
+      else
+         printf("Nao eh uma letra\n");
+
+      // Verifica se é um numero.
+      if (stra->is_numeric('1'))
+         printf("Eh um numero\n");
+
+      if (stra->is_numeric('s'))
+         printf("Eh um numero\n");
+      else
+         printf("Nao eh um numero\n");
+
+      // Reverte ordem dos caracteres da strings
+      stra->value(stra, "Minha string");
+      printf("str normal: %s\n", stra->string(stra));
+      printf("str revert: %s\n", stra->revert(stra));
+      
+      // Deleta string
+      stra->delete(stra);
+      printf("Error code: 0x%d\n\n", stra->error(stra));
+
+   }
+   else
+   {
+      if (stra != (sstring *)NULL)
+         printf("Error code: 0x%d\n", stra->error(stra));
+      else 
+         printf("Struct is NULL!\n");
+   }
+
+   return 0;
 }
